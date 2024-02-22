219bc9413a706f1eec11dee6273d656b4a746784
ottagit <ottaosawa@gmail.com>
2024-02-22 12:13:41 +0300

    upgrade db engine from m4 m5 instance
    Use AWS Secrets Manager for the MySQL DB credentials access.

--- live/prod/data-stores/mysql/main.tf
+++ live/prod/data-stores/mysql/main.tf
@@ -2,17 +2,27 @@ provider "aws" {
   region = "us-east-1"
 }
 
+locals {
+  db_creds = jsondecode(
+    data.aws_secretsmanager_secret_version.creds.secret_string
+  )
+}
+
+data "aws_secretsmanager_secret_version" "creds" {
+  secret_id = "prod-db-creds"
+}
+
 resource "aws_db_instance" "prod" {
   identifier_prefix = "prod-db"
   engine = "mysql"
-  allocated_storage = 20
-  instance_class = "db.m6i.large"
+  allocated_storage = 10
+  instance_class = "db.m5.large"
   skip_final_snapshot = true
   db_name = "proddb"
 
   # Set prod DB username and password
-  username = var.prod_db_username
-  password = var.prod_db_password
+  username = local.db_creds.username
+  password = local.db_creds.password
 }
 
 terraform {
--- live/prod/data-stores/mysql/variables.tf
+++ live/prod/data-stores/mysql/variables.tf
@@ -1,13 +1,13 @@
 # Define the username and password for the prod DB
 
-variable "prod_db_username" {
-  description = "The username of the prod DB"
-  type = string
-  sensitive = true
-}
+# variable "prod_db_username" {
+#   description = "The username of the prod DB"
+#   type = string
+#   sensitive = true
+# }
 
-variable "prod_db_password" {
-  description = "The password of the prod DB"
-  type = string
-  sensitive = true
-}
+# variable "prod_db_password" {
+#   description = "The password of the prod DB"
+#   type = string
+#   sensitive = true
+# }
