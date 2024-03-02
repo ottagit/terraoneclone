2c31a8a00e4325b7429e177017a419245347b556
ottagit <ottaosawa@gmail.com>
2024-03-02 11:37:34 +0300

    deploy MySQL DB replica in separate region

--- live/prod/data-stores/mysql/main.tf
+++ live/prod/data-stores/mysql/main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 locals {
   db_creds = jsondecode(
     data.aws_secretsmanager_secret_version.creds.secret_string
@@ -12,17 +8,39 @@ data "aws_secretsmanager_secret_version" "creds" {
   secret_id = "prod-db-creds"
 }
 
-resource "aws_db_instance" "prod" {
-  identifier_prefix = "prod-db"
-  engine = "mysql"
-  allocated_storage = 10
-  instance_class = "db.m5.large"
-  skip_final_snapshot = true
-  db_name = "proddb"
+provider "aws" {
+  region = "us-east-1"
+  alias = "primary"
+}
 
-  # Set prod DB username and password
-  username = local.db_creds.username
-  password = local.db_creds.password
+provider "aws" {
+  region = "ap-south-1"
+  alias = "replica"
+}
+
+module "mysql_primary" {
+  source = "github.com/ottagit/modules//data-stores/mysql?ref=v0.3.7"
+
+  providers = {
+    aws = aws.primary
+  }
+
+  db_name = "proddb"
+  db_username = local.db_creds.username
+  db_password = local.db_creds.password
+  # Must be enabled to support replication
+  backup_retention_period = 1
+}
+
+module "mysql_replica" {
+  source = "github.com/ottagit/modules//data-stores/mysql?ref=v0.3.7"
+
+  providers = {
+    aws = aws.replica
+  }
+
+  # Make this a replica of the primary
+  replicate_source_db = module.mysql_primary.arn
 }
 
 terraform {
